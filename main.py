bf4aacf5b7412f41c7ce110822bbb19ecf23698e
charleslimin <58681645+charleslimin@users.noreply.github.com>
2024-11-01 08:41:40 +0800

    Update Important Notice

--- main.py
+++ main.py
@@ -34,4 +34,13 @@ if form.form_submit_button("Submit"):
     st.write(response)
 #```
 
+with st.expander("Important Notice (Disclaimer)"):
+    st.write('''
+        This web application is a prototype developed for educational purposes only. The information provided here is NOT intended for real-world usage and should not be relied upon for making any decisions, especially those related to financial, legal, or healthcare matters.
+
+Furthermore, please be aware that the LLM may generate inaccurate or incorrect information. You assume full responsibility for how you use any generated output.
+
+Always consult with qualified professionals for accurate and personalized advice.
+    ''')
+    
 #------------------------------ END OF SCRIPT ------------------------------
